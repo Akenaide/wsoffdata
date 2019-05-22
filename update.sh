7d59388867ad239f6c1a151d5ed9f57b9e7aeb80
Akenaide <kevinms1@gmail.com>
2019-05-22 14:15:45 +0200

    no PR anymore

--- update.sh
+++ update.sh
@@ -2,15 +2,12 @@
 
 echo "yay"
 
-git fetch
-git checkout origin/master
+git pull
 
 wsoffcli fetch 
 MYDATE=`date -I`
-git checkout -b $MYDATE
 git add .
 git commit -m $MYDATE
 git push
-hub pull-request -m $MYDATE
 
 echo "end"
