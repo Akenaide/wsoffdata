def03f856a7b7c45cdfc88678273b66a66e30858
Akenaide <kevinms1@gmail.com>
2019-08-12 12:05:28 +0200

    having some issue with cron

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "yay"
 MYDATE=`date -I`
@@ -6,7 +6,7 @@ MYDATE=`date -I`
 git pull
 
 wsoffcli fetch && git add .
-git commit -m $MYDATE
-git push
+# git commit -m $MYDATE && echo "commit"
+# git push
 
 echo "end"
