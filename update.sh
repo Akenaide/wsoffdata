8c9310891d55836596616cca17b47dc434e1beee
Kevin MS <kevinms1@gmail.com>
2023-01-21 20:12:32 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ PWD=`pwd`
 
 git pull
 
-docker run --rm --user $(id -u) -v $PWD:/data akenaide/wsoffcli:master fetch -a && git add .
+docker run --rm --user "$(id -u):$(id -g)" -v $PWD:/data akenaide/wsoffcli:master fetch -a && git add .
 git commit -m $MYDATE && echo "commit"
 git push
 
