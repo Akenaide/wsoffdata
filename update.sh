cabb7c3d9ef597de8e55be79ca2bcef9f6645c52
Kevin MS <kevinms1@gmail.com>
2023-04-04 17:28:30 +0200

    Update update.sh
    Add docker pull

--- update.sh
+++ update.sh
@@ -3,10 +3,11 @@
 echo "yay"
 MYDATE=`date -I`
 PWD=`pwd`
+DOCKER_IMAGE = "akenaide/wsoffcli:master"
 
 git pull
-
-docker run --rm --user "$(id -u):$(id -g)" -v $PWD:/data akenaide/wsoffcli:master fetch -a && git add .
+docker pull $DOCKER_IMAGE
+docker run --rm --user "$(id -u):$(id -g)" -v $PWD:/data $DOCKER_IMAGE fetch -a && git add .
 git commit -m $MYDATE && echo "commit"
 git push
 
