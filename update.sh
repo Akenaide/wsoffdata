bf12737b13dc320687c24e10dd02e96aef728f55
Kevin MS <kevinms1@gmail.com>
2023-06-06 00:04:48 +0200

    Update update.sh
    Use args

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ DOCKER_IMAGE="akenaide/wsoffcli:master"
 
 git pull
 docker pull $DOCKER_IMAGE
-docker run --rm --user "$(id -u):$(id -g)" -v $PWD:/data $DOCKER_IMAGE fetch -a && git add .
+docker run -e WSOFF_REVERSE=$WSOFF_REVERSE -e WSOFF_ITER=$WSOFF_ITER --rm --user "$(id -u):$(id -g)" -v $PWD:/data $DOCKER_IMAGE fetch -a && git add .
 git commit -m $MYDATE && echo "commit"
 git push
 
